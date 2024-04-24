dfc74324358e99cd66678a46959dadca2310dcd4
WhatEverKid07 <eugeniosanc22@outlook.com>
2024-04-24 10:23:17 +0100

    Ready For Break Out Space

--- SEEL Source Code/Assets/Scripts/Buttons/ButtonHighlight.cs
+++ SEEL Source Code/Assets/Scripts/Buttons/ButtonHighlight.cs
@@ -2,12 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Target : MonoBehaviour
 {
     public Color outlineRed;
 
     public Renderer buttonHighlightRenderer;
+    public Transform buttonText;
     
     void Start()
     {
@@ -17,10 +19,17 @@ public class Target : MonoBehaviour
     private void OnMouseEnter()
     {
         buttonHighlightRenderer.material.color = outlineRed;
+        buttonText.gameObject.SetActive(true);
     }
 
     private void OnMouseExit()
     {
         buttonHighlightRenderer.material.color = Color.clear;
+        buttonText.gameObject.SetActive(false);
+    }
+
+    private void OnMouseDown()
+    {
+        buttonText.gameObject.SetActive(false);
     }
 }
